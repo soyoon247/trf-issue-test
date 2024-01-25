41b6709facf9f3d2c319e89a3e4a23afb73d03b3
soyoon <soyoon.dev@gmail.com>
2024-01-25 17:25:37 +0900

    SV-9670 auth/token/check, auth/token/refresh 추가
    SV-9670 all_headers 분리
    
    SV-9670 auth/token 리소스에 all_headers 적용

--- hwahae-api/deployment/main.tf
+++ hwahae-api/deployment/main.tf
@@ -6,3 +6,12 @@ resource "aws_api_gateway_deployment" "version_1_0" {
     create_before_destroy = true
   }
 }
+
+resource "aws_api_gateway_deployment" "version_1_1" {
+  rest_api_id = var.rest_api_id
+  description = "1.1 - /auth/token/check, auth/token/refresh 리소스 추가"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
---
+++ hwahae-api/resources/auth/token/main.tf
@@ -0,0 +1,19 @@
+module "token_resources" {
+  source      = "app.terraform.io/hh-devops/api-gateway-modules/aws"
+  version     = "1.0.0"
+  rest_api_id = var.args.rest_api_id
+  parent_id   = var.parent_id
+  path_part   = "token"
+}
+
+module "check" {
+  source    = "./check"
+  parent_id = module.token_resources.resource_id
+  args      = var.args
+}
+
+module "refresh" {
+  source    = "./refresh"
+  parent_id = module.token_resources.resource_id
+  args      = var.args
+}
