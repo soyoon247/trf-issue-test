d969849da1ac9c1ae062098dbe8333f1f78479a7
soyoon <soyoon.dev@gmail.com>
2024-01-26 17:48:21 +0900

    release/1.1 - search/reviews 이하 리소스 추가

--- hwahae-api/deployment/main.tf
+++ hwahae-api/deployment/main.tf
@@ -7,9 +7,9 @@ resource "aws_api_gateway_deployment" "version_1_0" {
   }
 }
 
-resource "aws_api_gateway_deployment" "version_1_1" {
+resource "aws_api_gateway_deployment" "release_1_1" {
   rest_api_id = var.rest_api_id
-  description = "1.1 - /auth/token/check, auth/token/refresh 리소스 추가"
+  description = "1.1 - /search/reviews, /search/reviews/count, /search/reviews/filters 리소스 추가"
 
   lifecycle {
     create_before_destroy = true
