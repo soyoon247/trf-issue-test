634b8b149b9a1a491e6eca4754863fb47091706b
soyoon <soyoon.dev@gmail.com>
2024-03-06 10:47:02 +0900

    SV-9645 commerce/goods/goods_index api 수정

--- hwahae-api/resources/commerce/goods/{goods_index}/relation-goods/main.tf
+++ hwahae-api/resources/commerce/goods/{goods_index}/relation-goods/main.tf
@@ -23,6 +23,17 @@ module "relation-goods_resources" {
         200 = {}
       }
     }
+    OPTIONS = {
+      integration_response_map = {
+        200 = {
+          response_parameters = {
+            "method.response.header.Access-Control-Allow-Headers" = "'Origin, Authorization, Content-Type, Content-Range, Content-Disposition, Content-Description, X-Requested-With, X-ACCESS_TOKEN, X-Amz-Date, X-Api-Key, X-Amz-Security-Token, Hwahae-User-Id, Hwahae-App-Version, Hwahae-Device-Scale, Hwahae-Timestamp, Hwahae-Platform, Hwahae-Signature, Hwahae-Device-Id'"
+            "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+            "method.response.header.Access-Control-Allow-Origin"  = var.args.all_origin
+          }
+        }
+      }
+    }
   }
 }
 
