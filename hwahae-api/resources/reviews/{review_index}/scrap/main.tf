4cdf1481131397b939f96c6ee3649cdea739ffe7
김소윤 <78311649+soyoon247@users.noreply.github.com>
2024-03-12 10:00:25 +0900

    SV-9993 reviews/idx/scraps -> scrap으로 변경 (#76)
    Co-authored-by: soyoon <soyoon.dev@gmail.com>

--- hwahae-api/resources/reviews/{review_index}/scrap/main.tf
+++ hwahae-api/resources/reviews/{review_index}/scrap/main.tf
@@ -1,9 +1,9 @@
-module "scraps_resources" {
+module "scrap_resources" {
   source      = "app.terraform.io/hh-devops/api-gateway-modules/aws"
   version     = "1.0.8"
   rest_api_id = var.args.rest_api_id
   parent_id   = var.parent_id
-  path_part   = "scraps"
+  path_part   = "scrap"
   common_values = {
     authorization = var.args.authorization.CUSTOM
     authorizer_id = var.args.hwahae_authorizer_id_map.hwahae_authorizer
@@ -18,7 +18,7 @@ module "scraps_resources" {
     integration_request_parameters = {
       "integration.request.path.review_index" = "method.request.path.review_index"
     }
-    integration_uri = "http://$${stageVariables.HWAHAE_SERVER_API_ALB}/$${stageVariables.version}/reviews/{review_index}/scraps"
+    integration_uri = "http://$${stageVariables.HWAHAE_SERVER_API_ALB}/$${stageVariables.version}/reviews/{review_index}/scrap"
   }
   method_values = {
     DELETE = {}
