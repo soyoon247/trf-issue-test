ff781eb783f0111bd20b2aac50749675acddba12
soyoon <soyoon.dev@gmail.com>
2024-03-29 09:50:48 +0900

    SV-10070 brands 하위 리소스 프록시 추가 (#95)
    # Conflicts:
    #	hwahae-api/resources/main.tf

---
+++ hwahae-api/resources/brands/{proxy+}/main.tf
@@ -0,0 +1,15 @@
+module "proxy_resources" {
+  source      = "app.terraform.io/hh-devops/api-gateway-modules/aws"
+  version     = "1.0.8"
+  rest_api_id = var.args.rest_api_id
+  parent_id   = var.parent_id
+  path_part   = "{proxy+}"
+
+  method_values = {
+    ANY = {
+      authorization   = var.args.authorization.CUSTOM
+      authorizer_id   = var.args.hwahae_authorizer_id_map.hwahae_authorizer_for_any_user
+      integration_uri = "http://$${stageVariables.HWAHAE_SERVER_API_ALB}/$${stageVariables.version}/brands/{proxy}"
+    }
+  }
+}
