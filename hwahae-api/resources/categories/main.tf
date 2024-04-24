0a5737bbee24a2b08409bbad75ca97b61b98a7a8
김선혜 <113883892+sunwise-kim@users.noreply.github.com>
2024-04-24 15:13:45 +0900

    SV-10462 /categories 에 OPTIONS 메소드 추가

--- hwahae-api/resources/categories/main.tf
+++ hwahae-api/resources/categories/main.tf
@@ -22,5 +22,16 @@ module "categories_resources" {
       integration_type = var.args.integration_type.HTTP
       integration_uri  = "http://$${stageVariables.HWAHAE_SERVER_API_ALB}/$${stageVariables.version}/categories"
     }
+    OPTIONS = {
+      integration_response_map = {
+        200 = {
+          response_parameters = {
+            "method.response.header.Access-Control-Allow-Headers" = var.args.cors_allowed_headers
+            "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+            "method.response.header.Access-Control-Allow-Origin"  = var.args.all_origin
+          }
+        }
+      }
+    }
   }
 }
