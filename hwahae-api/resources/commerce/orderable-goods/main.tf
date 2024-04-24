d38e376db7b72608a93503be6c40098d51bdc384
심소은 <se.shim@birdview.kr>
2024-04-24 09:35:48 +0900

    SV-10430 sale_goods API, commerce/orderable-goods API 의 options 메소드에 allow-header 추가

--- hwahae-api/resources/commerce/orderable-goods/main.tf
+++ hwahae-api/resources/commerce/orderable-goods/main.tf
@@ -22,7 +22,7 @@ module "orderable-goods_resources" {
       integration_response_map = {
         200 = {
           response_parameters = {
-            "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'"
+            "method.response.header.Access-Control-Allow-Headers" = "'Origin, Authorization, Content-Type, Content-Range, Content-Disposition, Content-Description, X-Requested-With, X-ACCESS_TOKEN, X-Amz-Date, X-Api-Key, X-Amz-Security-Token, Hwahae-User-Id, Hwahae-App-Version, Hwahae-Device-Scale, Hwahae-Timestamp, Hwahae-Platform, Hwahae-Signature, Hwahae-Device-Id'"
             "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
             "method.response.header.Access-Control-Allow-Origin"  = var.args.all_origin
           }
--- hwahae-api/resources/sale_goods/main.tf
+++ hwahae-api/resources/sale_goods/main.tf
@@ -35,7 +35,7 @@ module "sale_goods_resources" {
       integration_response_map = {
         200 = {
           response_parameters = {
-            "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+            "method.response.header.Access-Control-Allow-Headers" = "'Origin, Authorization, Content-Type, Content-Range, Content-Disposition, Content-Description, X-Requested-With, X-ACCESS_TOKEN, X-Amz-Date, X-Api-Key, X-Amz-Security-Token, Hwahae-User-Id, Hwahae-App-Version, Hwahae-Device-Scale, Hwahae-Timestamp, Hwahae-Platform, Hwahae-Signature, Hwahae-Device-Id'"
             "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
             "method.response.header.Access-Control-Allow-Origin"  = var.args.all_origin
           }
