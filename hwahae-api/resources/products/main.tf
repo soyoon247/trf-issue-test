ce59740d846f35c75c25c83a063c644bc499f78c
김선혜 <113883892+sunwise-kim@users.noreply.github.com>
2024-04-24 09:33:13 +0900

    SV-10431 /products 에 OPTIONS 메소드 추가

--- hwahae-api/resources/products/main.tf
+++ hwahae-api/resources/products/main.tf
@@ -34,6 +34,17 @@ module "products_resources" {
       integration_type = var.args.integration_type.HTTP
       integration_uri  = "http://$${stageVariables.HWAHAE_SERVER_API_ALB}/$${stageVariables.version}/products"
     }
+    OPTIONS = {
+      integration_response_map = {
+        200 = {
+          response_parameters = {
+            "method.response.header.Access-Control-Allow-Headers" = var.args.cors_allowed_headers
+            "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+            "method.response.header.Access-Control-Allow-Origin"  = var.args.all_origin
+          }
+        }
+      }
+    }
   }
 }
 
