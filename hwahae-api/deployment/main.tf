3cfe94154bcd6b731317d08de96aeb5045cbe893
soyoon <soyoon.dev@gmail.com>
2024-05-03 14:13:24 +0900

    SV-10566 1차 권한부여자 이관, deployment 8.1 생성

--- hwahae-api/deployment/main.tf
+++ hwahae-api/deployment/main.tf
@@ -69,3 +69,12 @@ resource "aws_api_gateway_deployment" "release_8_0" {
     create_before_destroy = true
   }
 }
+
+resource "aws_api_gateway_deployment" "release_8_1" {
+  rest_api_id = var.rest_api_id
+  description = "8.1 - 1차 권한부여자 변경"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
--- hwahae-api/resources/users/me/favorite/ingredients/main.tf
+++ hwahae-api/resources/users/me/favorite/ingredients/main.tf
@@ -8,7 +8,7 @@ module "ingredients_resources" {
   method_values = {
     GET = {
       authorization = var.args.authorization.CUSTOM
-      authorizer_id = var.args.hwahae_authorizer_id_map.temp_hwahae_authorizer
+      authorizer_id = var.args.hwahae_authorizer_id_map.hwahae_authorizer
       method_response_map = {
         200 = { response_models = var.args.empty_response_models }
       }
