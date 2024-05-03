c077ae52fb13dac9074792295f54af6a1f71693b
soyoon <soyoon.dev@gmail.com>
2024-05-03 14:13:24 +0900

    SV-10566 1차 권한부여자 이관, deployment 8.4 생성

--- hwahae-api/resources/users/me/favorite/ingredient-folders/{ingredient_favorite_folder_id}/ingredients/main.tf
+++ hwahae-api/resources/users/me/favorite/ingredient-folders/{ingredient_favorite_folder_id}/ingredients/main.tf
@@ -6,7 +6,7 @@ module "ingredients_resources" {
   path_part   = "ingredients"
   common_values = {
     authorization = var.args.authorization.CUSTOM
-    authorizer_id = var.args.hwahae_authorizer_id_map.temp_hwahae_authorizer
+    authorizer_id = var.args.hwahae_authorizer_id_map.hwahae_authorizer
     method_response_map = {
       200 = { response_models = var.args.empty_response_models }
     }
--- hwahae-api/resources/users/me/history/ingredients/main.tf
+++ hwahae-api/resources/users/me/history/ingredients/main.tf
@@ -7,7 +7,7 @@ module "ingredients_resources" {
 
   common_values = {
     authorization = var.args.authorization.CUSTOM
-    authorizer_id = var.args.hwahae_authorizer_id_map.temp_hwahae_authorizer
+    authorizer_id = var.args.hwahae_authorizer_id_map.hwahae_authorizer
     method_response_map = {
       200 = { response_models = var.args.empty_response_models }
     }
