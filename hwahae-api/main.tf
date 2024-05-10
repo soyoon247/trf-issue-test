4f682c81ead9f48ce88c473b7c0a6c191241152f
soyoon <soyoon.dev@gmail.com>
2024-05-10 10:50:05 +0900

    source change

--- hwahae-api/main.tf
+++ hwahae-api/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "sy_trf_test"
 
     workspaces {
@@ -10,7 +10,7 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
