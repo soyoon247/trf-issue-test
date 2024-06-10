4b152b94d25459032478eec66f62728ac5c56c24
soyoon <soyoon.dev@gmail.com>
2024-06-11 08:31:26 +0900

    delete depends_on

--- hwahae-api/main.tf
+++ hwahae-api/main.tf
@@ -37,7 +37,7 @@ module "hwahae-rest-api-resources" {
 module "deployment" {
   source      = "./deployment"
   rest_api_id = data.aws_api_gateway_rest_api.hwahae_api.id
-  depends_on  = [module.hwahae-rest-api-resources]
+#  depends_on  = [module.hwahae-rest-api-resources]
 }
 
 
