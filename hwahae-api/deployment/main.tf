7b20ceb6a6bfb0c6a47f6dcc8c262e1c6ecd5d32
soyoon <soyoon.dev@gmail.com>
2024-06-10 16:25:16 +0900

    create before destroy edit

--- hwahae-api/deployment/main.tf
+++ hwahae-api/deployment/main.tf
@@ -1,8 +1,8 @@
 resource "aws_api_gateway_deployment" "release_test" {
   rest_api_id = var.rest_api_id
   description = "test"
-
-  lifecycle {
-    create_before_destroy = true
-  }
+#
+#  lifecycle {
+#    create_before_destroy = true
+#  }
 }
