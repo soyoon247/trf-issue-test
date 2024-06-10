ea1a2f3addc84c75f1349de06dec0676e533a3ea
soyoon <soyoon.dev@gmail.com>
2024-06-11 08:22:02 +0900

    add deployment

--- hwahae-api/deployment/main.tf
+++ hwahae-api/deployment/main.tf
@@ -6,3 +6,12 @@ resource "aws_api_gateway_deployment" "release_test" {
 #    create_before_destroy = true
 #  }
 }
+
+resource "aws_api_gateway_deployment" "release_test_2" {
+  rest_api_id = var.rest_api_id
+  description = "test 2"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
