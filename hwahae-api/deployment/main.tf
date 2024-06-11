fdc01e2b9de829aff46b3133a133f98231c8b479
soyoon <soyoon.dev@gmail.com>
2024-06-11 09:04:31 +0900

    add trigger

--- hwahae-api/deployment/main.tf
+++ hwahae-api/deployment/main.tf
@@ -11,6 +11,10 @@ resource "aws_api_gateway_deployment" "release_test_2" {
   rest_api_id = var.rest_api_id
   description = "test 2"
 
+  triggers = {
+    redeployment = 1
+  }
+
   lifecycle {
     create_before_destroy = true
   }
