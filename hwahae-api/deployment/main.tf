695950aba042abf8e067cd1fc76a7ad042ee9b1f
soyoon <soyoon.dev@gmail.com>
2024-06-11 09:10:02 +0900

    delete deployment 2

--- hwahae-api/deployment/main.tf
+++ hwahae-api/deployment/main.tf
@@ -7,15 +7,6 @@ resource "aws_api_gateway_deployment" "release_test" {
 #  }
 }
 
-resource "aws_api_gateway_deployment" "release_test_2" {
-  rest_api_id = var.rest_api_id
-  description = "test 2"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-}
 
 resource "aws_api_gateway_deployment" "release_test_3" {
   rest_api_id = var.rest_api_id
